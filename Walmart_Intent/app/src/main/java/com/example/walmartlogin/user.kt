63829144f53a908f68571bae7710c467c628756b
MulualemBerhanu <89715104+MulualemBerhanu@users.noreply.github.com>
2021-10-11 11:22:43 -0500

    Intent

--- Walmart_Intent/app/src/main/java/com/example/walmartlogin/user.kt
+++
@@ -1,7 +0,0 @@
-package com.example.walmartlogin
-
-import java.io.Serializable
-
-data class User (var firstName:String,var lastName:String?,var email:String,var password:String):Serializable  {
-
-}
